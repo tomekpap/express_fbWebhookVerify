3e521d3d3f33a05287cf2a673340be06ea9b8418
tomekpap <tomekpapierniak@gmail.com>
2021-08-13 09:27:12 +0200

    added index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports = function fbWebhookVerify(req, res, next) {
+  // Parse the query params
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  // Check if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Check the mode and token sent is correct
+    if (mode === "subscribe" && token === config.verifyToken) {
+      // Respond with the challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Respond with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  } else {
+    console.warn("Got /webhook but without needed parameters.");
+  }
+  next();
+};
